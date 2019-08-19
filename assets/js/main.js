15319a385a967228b6af0d0fb6aec2c2e74cce4f
CamilaRivera <camila.riverart@gmail.com>
2019-08-19 04:52:40 +0000

    refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,117 @@
-// function getChar() {
-//   var div = document.createElement("div");
-//   div.style.width = "100px";
-//   div.style.height = "100px";
-//   div.style.background = "red";
-//   div.style.color = "white";
+//Bar options
+var data = {
+  values: [[4, 5], [18, 19, 29], [2]],
+  labels: ["red", "azul", "verde"]
+};
+var options = {
+  width: 250,
+  height: 250,
+  dataColors: [["red", "blue"], ["green", "red", "blue"], ["purple"]],
+  spacing: 25,
+  // start , bottom, center
+  barLabelPosition: 'center',
+  titleOptions: {
+    text: 'Hello Chart!',
+    color: 'red',
+    size: 36
+  }
+};
+var chartContainer = $('#chart');
 
-//   document.getElementById("main").appendChild(div);
-// }
 
-var data = [4, 5, 18, 19, 29];
-var options = { width: 250, height: 250};
-var widthPerData = (options.width / data.length) * 0.8;
-var height = 750;
+function sumArray(data) {
+  sumTotal = 0;
+  for (var i = 0; i < data.length; i++) {
+    sumTotal = sumTotal + data[i];
+  }
 
-function createDiv(theWidth, theHeight, color) {
-
-  $('<div class=\'bg-primary\' style=\'width:' + theWidth + 'px;height:' + theHeight + '%;\'/>').appendTo('#chart');
-  // $('<div class=\'btn bg-primary\' style=\'width:' + theWidth + 'px;height:' + theHeight + 'px;background-color:' + color
-  // ';\'/>').appendTo('#main');
-
+  return sumTotal;
 }
 
-function createBars() {
-  var values = [4, 5, 18, 19, 29];
-  var maxValue = Math.max.apply(null, values);
-  var maxHeight = maxValue;
+function getMaxBar(data) {
+  var barSums = data.map(sumArray);
+  return Math.max.apply(null, barSums);
+}
 
 
-  for (var i = 0; i < values.length; i++) {
-    var heightPerData = (values[i] / maxHeight) * 100;
-    createDiv(widthPerData, heightPerData, 'red');
+function createBar(barContainerElement, barOptions) {
+  var bar = $('<div></div>')
+    .addClass("text-center")
+    .css('width', barOptions.width + 'px' )
+    .css('height', barOptions.height + '%')
+    .css('margin-left', barOptions.spacing + 'px');
+  bar.appendTo(barContainerElement);
+
+  for (var i = 0; i < barOptions.values.length; i++) {
+    var height = (barOptions.values[i] / sumArray(barOptions.values)) * 100;
+    $('<div></div>')
+      .addClass('bar-chart-bar')
+      .addClass('text-white')
+      .addClass('justify-content-' + barOptions.labelPosition)
+      .addClass('d-flex')
+      .addClass('flex-column')
+      .css('height', height + '%')
+      .css('background-color', barOptions.dataColorBar[i])
+      .text(barOptions.text[i])
+      .appendTo(bar);
   }
 }
 
-function sizeChart(width, height){
-  $('#chart').height(height);
-  $('#chart').width(width);
+function createLabel(barOptions) {
+  // var span = $('<div class= justify-content + barOptions.labelPosition + ' text-center\' style=\'width:' + barOptions.width + 'px;\'/>');
+  $('<div></div>')
+    .addClass('justify-content-' + barOptions.labelPosition)
+    .addClass('text-center')
+    .css('width', barOptions.width + 'px')
+    .css('margin-left', barOptions.spacing + 'px')
+    .text(barOptions.labelName)
+    .appendTo('#label');
 }
 
-createBars();
-sizeChart(350, 350);
+function sizeChart(width, height) {
+  $('#chart').height(height);
+  $('#chart').width(width);
+  $('#label').width(width);
+}
+
+function setTitle(title, color, size) {
+  var chartTitle = document.getElementById("chartTitle");
+  chartTitle.innerHTML = title;
+  chartTitle.style.fontSize = size + "px";
+  chartTitle.style.color = color;
+}
+
+function createBars(data, options, element) {
+
+  sizeChart(options.width, options.height);
+  setTitle(options.titleOptions.text, options.titleOptions.color, options.titleOptions.size);
+
+  var widthAvailableForBars = options.width - options.spacing * (data.values.length + 1);
+  var barWidth = widthAvailableForBars / data.values.length;
+
+  var labels = data.labels;
+  var maxValue = getMaxBar(data.values);
+  var maxHeight = maxValue;
+
+
+  for (var i = 0; i < data.values.length; i++) {
+    var barOptions = {
+      width: barWidth,
+      height: (sumArray(data.values[i]) / maxHeight) * 100,
+      text: data.values[i],
+      labelPosition: options.barLabelPosition,
+      labelName: labels[i],
+      values: data.values[i],
+      dataColorBar: options.dataColors[i],
+      spacing: options.spacing
+    };
+    createBar(element, barOptions);
+    createLabel(barOptions);
+  }
+}
 
 
 
-// drawBarChart(data, options, element);
-// $('#main').append(
-//   $('<div/>')
-//     .attr("id", "newDiv1")
-//     .addClass("newDiv purple bloated")
-//     .css('background-color', 'red')
-//     .append("<span/>")
-//     .text("hello world")
-// );
+
+createBars(data, options, chartContainer );
+
